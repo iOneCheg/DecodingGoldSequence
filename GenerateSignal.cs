c8ec935218bab2ba9f290e0251c107430df11759
TorunovIvan <torunov99@gmail.com>
2023-10-30 10:14:01 +0300

    Добавьте файлы проекта.

---
+++ GenerateSignal.cs
@@ -0,0 +1,104 @@
+﻿using DevExpress.XtraPrinting.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodingGoldSequence
+{
+    internal class GenerateSignal
+    {
+        private Random? _randomGenerator;
+        public List<PointD> I, Q;
+        public List<PointD> ComplexEnvelope;
+        public int BitsCount { get; set; }
+        public int SampleFreq { get; set; }
+        public int CarrierFreq { get; set; }
+        public int BaudRate { get; set; }
+        public double dt => 1d / (double)(SampleFreq * 1000);
+        public int TBit => (SampleFreq * 1000) / BaudRate;
+        public int[] BitSequence
+        {
+            get
+            {
+                _randomGenerator = new Random(Guid.NewGuid().GetHashCode());
+                return Enumerable
+               .Range(0, BitsCount)
+               .Select(i => Convert.ToInt32(_randomGenerator.Next(2) == 0))
+               .ToArray();
+            }
+        }
+
+        public void GetIQComponents(int[] GoldBits)
+        {
+            I = new List<PointD>();
+            Q = new List<PointD>();
+            ComplexEnvelope = new List<PointD>();
+
+            int counter = 0;
+            for (int i = 0; i < GoldBits.Length; i += 2)
+            {
+                for (int j = 0; j < TBit * 2; j++)
+                {
+                    I.Add(new PointD(counter * dt, GoldBits[i] - 0.5));
+                    Q.Add(new PointD(counter * dt, GoldBits[i + 1] - 0.5));
+                    double temp = (GoldBits[i] - 0.5) * Math.Cos(2 * Math.PI * CarrierFreq * counter * dt) -
+                        (GoldBits[i + 1] - 0.5) * Math.Sin(2 * Math.PI * CarrierFreq * counter * dt);
+                    ComplexEnvelope.Add(new PointD(counter * dt, temp));
+                    counter++;
+                }
+            }
+        }
+        /// <summary>
+        /// Наложение шума на сигнал
+        /// </summary>
+        /// <param name="snrDb">SNR в дБ</param>
+        public void MakeNoise(double snrDb)
+        {
+
+            // Наложение шума на исследуемый сигнал.
+            ComplexEnvelope = ComplexEnvelope.Zip(
+                    GenerateNoise(ComplexEnvelope.Count, ComplexEnvelope.Sum(p => p.Y * p.Y), snrDb),
+                    (p, n) => new PointD(p.X, p.Y + n))
+                .ToList();
+        }
+        /// <summary>
+        /// Генерация случайного числа по нормальному распределению
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static double GetNormalRandom(double min, double max, int n = 12)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            var sum = 0d;
+            for (var i = 0; i < n; i++)
+                sum += rnd.NextDouble() * (max - min) + min;
+            return sum / n;
+        }
+        /// <summary>
+        /// Генерация белого гауссовского шума
+        /// </summary>
+        /// <param name="countNumbers">Число отсчетов</param>
+        /// <param name="energySignal">Энергия сигнала</param>
+        /// <param name="snrDb">SNR в дБ</param>
+        /// <returns></returns>
+        private static IEnumerable<double> GenerateNoise(int countNumbers, double energySignal, double snrDb)
+        {
+            var noise = new List<double>();
+            for (var i = 0; i < countNumbers; i++)
+                noise.Add(GetNormalRandom(-1d, 1d));
+
+            // Нормировка шума.
+            var snr = Math.Pow(10, -snrDb / 10);
+            var norm = Math.Sqrt(snr * energySignal / noise.Sum(y => y * y));
+
+            return noise.Select(y => y * norm).ToList();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using ScottPlot.Control;
+using ScottPlot;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DecodingGoldSequence
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, int[]> _goldSequences;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadGoldSequences();
+
+        }
+
+        private void OnLoadedMainWindow(object sender, RoutedEventArgs e)
+        {
+            SetUpChart(ChartIComponent, "I", "Время, с", "Амплитуда");
+            SetUpChart(ChartQComponent, "Q", "Время, с", "Амплитуда");
+            SetUpChart(ChartComplexSignal, "Комплексная огибающая", "Время, с", "Амплитуда");
+        }
+
+
+        private void OnClickGenerateSignals(object sender, RoutedEventArgs e)
+        {
+            GenerateSignal _gS = new GenerateSignal();
+
+            _gS.BitsCount = (int)BitsCount.Value;
+            _gS.SampleFreq = (int)SamplingFreq.Value;
+            _gS.BaudRate = (int)BaudRate.Value;
+            _gS.CarrierFreq = 500;
+            int[] bits = _gS.BitSequence;
+
+            var builder = new StringBuilder();
+            Array.ForEach(bits, x => builder.Append(x));
+            string s = builder.ToString();
+            InputBits.Text = s;
+            int[] res = GoldCodes.ConvertToGoldSequence(bits, _goldSequences);
+            _gS.GetIQComponents(res);
+            _gS.MakeNoise((double)SNR.Value);
+
+           ChartIComponent.Plot.Clear();
+            ChartIComponent.Plot.AddSignalXY(_gS.I.Select(p => p.X).ToArray(),
+                _gS.I.Select(p => p.Y).ToArray(), color: Color.Blue);
+            ChartIComponent.Refresh();
+
+            ChartQComponent.Plot.Clear();
+            ChartQComponent.Plot.AddSignalXY(_gS.Q.Select(p => p.X).ToArray(),
+                _gS.Q.Select(p => p.Y).ToArray(), color: Color.Blue);
+            ChartQComponent.Refresh();
+
+            ChartComplexSignal.Plot.Clear();
+            ChartComplexSignal.Plot.AddSignalXY(_gS.ComplexEnvelope.Select(p => p.X).ToArray(),
+                _gS.ComplexEnvelope.Select(p => p.Y).ToArray(), color: Color.Blue);
+            ChartComplexSignal.Refresh();
+
+        }
+        private void LoadGoldSequences()
+        {
+            if (_goldSequences == null)
+            {
+                int[] MSequence1 = GoldCodes.GenerateMSequence(new int[] { 1, 0, 0, 0, 0, 1 });
+                int[] MSequence2 = GoldCodes.GenerateMSequence(new int[] { 1, 1, 0, 0, 1, 1 });
+
+                _goldSequences = new Dictionary<string, int[]>
+                {
+                    ["00"] = GoldCodes.GetGoldCode(MSequence1, MSequence2),
+                    ["10"] = GoldCodes.GetGoldCode(MSequence1, GoldCodes.ShiftedArray(MSequence2, 1)),
+                    ["01"] = GoldCodes.GetGoldCode(MSequence1, GoldCodes.ShiftedArray(MSequence2, 2)),
+                    ["11"] = GoldCodes.GetGoldCode(MSequence1, GoldCodes.ShiftedArray(MSequence2, 3))
+                };
+            }
+        }
+        private static void SetUpChart(IPlotControl chart, string title, string labelX, string labelY)
+        {
+            chart.Plot.Title(title);
+            chart.Plot.XLabel(labelX);
+            chart.Plot.YLabel(labelY);
+            chart.Plot.XAxis.MajorGrid(enable: true, color: Color.FromArgb(50, Color.Black));
+            chart.Plot.YAxis.MajorGrid(enable: true, color: Color.FromArgb(50, Color.Black));
+            chart.Plot.XAxis.MinorGrid(enable: true, color: Color.FromArgb(30, Color.Black), lineStyle: LineStyle.Dot);
+            chart.Plot.YAxis.MinorGrid(enable: true, color: Color.FromArgb(30, Color.Black), lineStyle: LineStyle.Dot);
+            chart.Plot.Margins(x: 0.0, y: 0.8);
+            chart.Plot.SetAxisLimits(xMin: 0);
+            chart.Configuration.Quality = QualityMode.High;
+            chart.Configuration.DpiStretch = false;
+            chart.Refresh();
+        }
+    }
+}
